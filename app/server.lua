8f04de78af6539ca9e1874c9eef5a6e04a82c923
zhanyuan <Zhanyuan.Feng@partner.bmw.com>
2019-07-12 10:10:47 +0800

    first commit

---
+++ app/server.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: QXU8000
+-- Date: 2019/7/11
+-- Time: 17:55
+-- To change this template use File | Settings | File Templates.
+--
+
+
+local lor = require("lor.index")
+local app = lor()
+
+app:get("/index", function(req, res, next)
+    res:send("hello world!")
+end)
+
+return app
+
